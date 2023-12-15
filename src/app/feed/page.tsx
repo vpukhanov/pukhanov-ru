05b434d66396004dea5f50351dedaca754b241a6
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2023-12-15 09:23:45 +0300

    feat: add feed page

---
+++ src/app/feed/page.tsx
@@ -0,0 +1,21 @@
+import { Metadata } from 'next';
+import { FeedMetadata, feed } from '@/lib/feed';
+
+export const metadata: Metadata = {
+  title: "Лента",
+};
+
+export default async function Feed() {
+	const items = await feed();
+	return <div>
+		{items.map(i => <FeedItem key={i.slug} {...i} />)}	
+	</div>
+}
+
+async function FeedItem({slug, metadata}: { slug: string; metadata: FeedMetadata }) {
+	const Content = (await import(`@/app/(prose)/feed/${slug}/page.mdx`)).default
+	return <article className="prose prose-h1:text-lg prose-h1:font-semibold mb-10">
+		<time className="text-sm text-slate-500" dateTime={metadata.datePublished}>{metadata.datePublished}</time>
+		<Content />
+	</article>
+}
