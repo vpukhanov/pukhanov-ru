12ad8f3986e262e3ef1773604be71a2b760e900d
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2023-12-15 10:05:11 +0300

    feat: add atom xml for feed

--- src/lib/atom.ts
+++ src/lib/atom.ts
@@ -1,9 +1,10 @@
 import { Feed } from "feed";
 import { posts } from "./posts";
+import { feed } from "./feed";
 
 export async function postsAtom() {
   const f = new Feed({
-    title: "Вячеслав Пуханов",
+    title: "Блог | Вячеслав Пуханов",
     id: "https://pukhanov.ru/posts/feed.xml",
     favicon: "https://pukhanov.ru/favicon.ico",
     copyright: "© Вячеслав Пуханов, с 2013 года",
@@ -24,6 +25,37 @@ export async function postsAtom() {
       title: post.metadata.title,
       description: post.metadata.description,
       link: `https://pukhanov.ru/posts/${post.slug}`,
+      date: new Date(post.metadata.datePublished),
+    });
+  });
+
+  f.addCategory("technology");
+
+  return f.atom1();
+}
+
+export async function feedAtom() {
+  const f = new Feed({
+    title: "Лента | Вячеслав Пуханов",
+    id: "https://pukhanov.ru/feed/feed.xml",
+    favicon: "https://pukhanov.ru/favicon.ico",
+    copyright: "© Вячеслав Пуханов, с 2013 года",
+    feedLinks: {
+      atom: "https://pukhanov.ru/feed/feed.xml",
+    },
+    author: {
+      name: "Вячеслав Пуханов",
+      email: "vyacheslav@pukhanov.ru",
+      link: "https://pukhanov.ru",
+    },
+  });
+
+  const list = await feed();
+
+  list.forEach((post) => {
+    f.addItem({
+      title: post.metadata.title,
+      link: `https://pukhanov.ru/feed/${post.slug}`,
       date: new Date(post.metadata.datePublished),
     });
   });
