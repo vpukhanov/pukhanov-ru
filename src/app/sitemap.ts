4c49b0bc1d251a7e8c59a8e42f2c81c5386bb4ce
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2023-12-15 11:03:49 +0300

    feat: add feed to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -14,6 +14,10 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       url: "https://pukhanov.ru/posts",
       priority: 0.8,
     },
+    {
+      url: "https://pukhanov.ru/feed",
+      priority: 0.7,
+    },
     ...list.map((post) => ({
       url: `https://pukhanov.ru/posts/${post.slug}`,
     })),
