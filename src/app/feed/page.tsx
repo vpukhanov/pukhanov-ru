55bc9dbe6867cd0717ddf816c6b0fb762d356fc5
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2023-12-15 10:47:53 +0300

    chore: run prettier on files

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,23 +1,40 @@
-import { Metadata } from 'next';
-import { FeedMetadata, feed } from '@/lib/feed';
-import FeedNotice from '@/components/feed-notice';
+import { Metadata } from "next";
+import { FeedMetadata, feed } from "@/lib/feed";
+import FeedNotice from "@/components/feed-notice";
 
 export const metadata: Metadata = {
   title: "Лента",
 };
 
 export default async function Feed() {
-	const items = await feed();
-	return <div>
-		<FeedNotice />
-		{items.map(i => <FeedItem key={i.slug} {...i} />)}	
-	</div>
+  const items = await feed();
+  return (
+    <div>
+      <FeedNotice />
+      {items.map((i) => (
+        <FeedItem key={i.slug} {...i} />
+      ))}
+    </div>
+  );
 }
 
-async function FeedItem({slug, metadata}: { slug: string; metadata: FeedMetadata }) {
-	const Content = (await import(`@/app/(prose)/feed/${slug}/page.mdx`)).default
-	return <article className="prose prose-h1:text-lg prose-h1:font-semibold mb-10">
-		<time className="block text-sm font-light mb-1" dateTime={metadata.datePublished}>{metadata.datePublished}</time>
-		<Content />
-	</article>
+async function FeedItem({
+  slug,
+  metadata,
+}: {
+  slug: string;
+  metadata: FeedMetadata;
+}) {
+  const Content = (await import(`@/app/(prose)/feed/${slug}/page.mdx`)).default;
+  return (
+    <article className="prose mb-10 prose-h1:text-lg prose-h1:font-semibold">
+      <time
+        className="mb-1 block text-sm font-light"
+        dateTime={metadata.datePublished}
+      >
+        {metadata.datePublished}
+      </time>
+      <Content />
+    </article>
+  );
 }
