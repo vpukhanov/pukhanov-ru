add0a3825336e7a7018c32cb9aaac0cc94732f70
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2024-06-05 20:43:51 +0300

    feat: combine feed and posts into one stream

--- src/app/feed/feed.xml/route.ts
+++
@@ -1,9 +0,0 @@
-import { feedAtom } from "@/lib/atom";
-
-export async function GET() {
-  return new Response(await feedAtom(), {
-    headers: {
-      "Content-Type": "text/xml; charset=utf-8",
-    },
-  });
-}
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,10 +1,9 @@
 import { MetadataRoute } from "next";
 
-import { feed } from "@/lib/feed";
 import { posts } from "@/lib/posts";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const [postItems, feedItems] = await Promise.all([posts(), feed()]);
+  const postItems = await posts();
 
   return [
     {
@@ -15,15 +14,8 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       url: "https://pukhanov.ru/posts",
       priority: 0.8,
     },
-    {
-      url: "https://pukhanov.ru/feed",
-      priority: 0.7,
-    },
     ...postItems.map((post) => ({
       url: `https://pukhanov.ru/posts/${post.slug}`,
-    })),
-    ...feedItems.map((item) => ({
-      url: `https://pukhanov.ru/feed/${item.slug}`,
     })),
   ];
 }
