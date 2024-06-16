bb72bb3ad78827f745106160a1dbaee1605d293b
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2024-06-16 07:40:31 +0300

    fix: remove the unused is-list group

--- src/components/date-published.tsx
+++ src/components/date-published.tsx
@@ -1,7 +1,7 @@
 export default function DatePublished({ dateTime }: { dateTime: string }) {
   return (
-    <small className="group-[.is-list]:hidden">
-      <time dateTime={dateTime}>Published on {dateTime}</time>
-    </small>
+    <time dateTime={dateTime} className="text-sm">
+      Published on {dateTime}
+    </time>
   );
 }
