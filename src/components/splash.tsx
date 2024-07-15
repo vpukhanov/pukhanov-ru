e31d27413e3eb37d5adf298aff4598ce35c93d36
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2024-07-16 00:11:34 +0300

    feat: adjust hero-title, splash and wave animations and colors

--- src/components/splash.tsx
+++ src/components/splash.tsx
@@ -3,11 +3,12 @@ import styles from "./splash.module.css";
 export default function Splash() {
   return (
     <div className={styles.root}>
-      <div className={`${styles.color} ${styles.pink}`}></div>
-      <div className={`${styles.color} ${styles.red}`}></div>
-      <div className={`${styles.color} ${styles.yellow}`}></div>
-      <div className={`${styles.color} ${styles.green}`}></div>
-      <div className={`${styles.color} ${styles.blue}`}></div>
+      <div className={`${styles.stripe} ${styles.red}`}></div>
+      <div className={`${styles.stripe} ${styles.orange}`}></div>
+      <div className={`${styles.stripe} ${styles.yellow}`}></div>
+      <div className={`${styles.stripe} ${styles.green}`}></div>
+      <div className={`${styles.stripe} ${styles.blue}`}></div>
+      <div className={`${styles.stripe} ${styles.purple}`}></div>
     </div>
   );
 }
